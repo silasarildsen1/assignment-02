bc320ed09a8e45aee8f6774fe2d123afb47fcf69
Silas Arildsen <silas.r.arildsen@gmail.com>
2022-09-20 16:39:38 +0200

    One-liner extension methods

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,19 +2,6 @@ namespace Assignment2;
 
 public static class Extensions
 {
-    public static IEnumerable<T> Flatten<T>(this IEnumerable<T>[] items) {
-        foreach (IEnumerable<T> item in items) {
-            foreach (T actual in item) {
-                yield return actual;
-            }
-        }
-    }
-
-    public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate) {
-        foreach (var item in items) {
-            if (predicate(item)) {
-                yield return item;
-            }
-        }
-    }
+    public static IEnumerable<T> Flatten<T>(this IEnumerable<T>[] items) => items.SelectMany(item => item);
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate) => items.Where(new Func<T, bool>(predicate));
 }
