79bfd4470e5054416b4eccdf9cd6538be489bb84
Tobias Gad Spoorendonk <tosp@itu.dk>
2022-09-20 14:36:14 +0200

    Reverted to working git patch

--- Students.Tests/StudentTest.cs
+++ Students.Tests/StudentTest.cs
@@ -2,33 +2,19 @@ namespace Students.Tests;
 
 public class StudentTest
 {
-    [Fact]
-    public void Student_Active_ToString()
+        [Fact]
+    public void ImmmutableStudent_Active_ToString()
     {
-        var s = new Student(1);
-        s.GivenName = "Silas";
-        s.Surname = "Arildsen";
-        s.StartDate = new DateTime(2021, 8, 1);
-        s.EndDate = new DateTime(2024, 6, 1);
-        s.GraduationDate = new DateTime(2024, 6, 1);
-
-        var actual = s.ToString();
-        var expected = "Silas Arildsen, ID: 1. Start: 01/08/2021. Status: Active. Graduates: 01/06/2024.";
-        actual.Should().Be(expected);
+        var s = new ImmutableStudent(1, "Silas", "Arildsen", Students.StudentStatus.New,  new DateTime(2021, 8, 1), new DateTime(2024, 6, 1), new DateTime(2024, 6, 1));
+        var expected = "ImmutableStudent { Id = 1, GivenName = Silas, Surname = Arildsen, Status = New, StartDate = 01/08/2021 00:00:00, EndDate = 01/06/2024 00:00:00, GraduationDate = 01/06/2024 00:00:00 }";
+        s.ToString().Should().Be(expected);
     }
 
     [Fact]
-    public void Student_Graduated_ToString()
-    {
-        var s = new Student(1);
-        s.GivenName = "Silas";
-        s.Surname = "Arildsen";
-        s.StartDate = new DateTime(2016, 8, 1);
-        s.EndDate = new DateTime(2019, 6, 1);
-        s.GraduationDate = new DateTime(2019, 6, 1);
-        
-        var actual = s.ToString();
-        var expected = "Silas Arildsen, ID: 1. Start: 01/08/2016. Status: Graduated. Graduates: 01/06/2019.";
-        actual.Should().Be(expected);
+    public void ImmstaticStudent_Equality() {
+        var s1 = new ImmutableStudent(1, "Silas", "Arildsen", Students.StudentStatus.New,  new DateTime(2021, 8, 1), new DateTime(2024, 6, 1), new DateTime(2024, 6, 1));
+        var s2 = new ImmutableStudent(1, "Silas", "Arildsen", Students.StudentStatus.New,  new DateTime(2021, 8, 1), new DateTime(2024, 6, 1), new DateTime(2024, 6, 1));
+        (s1 == s2).Should().Be(true);
+        s1.Should().Be(s2);
     }
 }
